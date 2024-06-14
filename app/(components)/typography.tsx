b4fc4c92043da85354aa8e2c8c326bbc09dee49f
cf913 <chaddudz@hotmail.fr>
2024-06-14 21:24:51 +1000

    typography

--- app/(components)/typography.tsx
+++ app/(components)/typography.tsx
@@ -1,10 +1,27 @@
-import {Container, Content, Header} from '@/components/ui'
+import {Container, Content, Header, ThemedText} from '@/components/ui'
+
+const TYPES: {
+  type: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'small' | 'link'
+  size: number
+}[] = [
+  {type: 'title', size: 32},
+  {type: 'subtitle', size: 20},
+  {type: 'defaultSemiBold', size: 16},
+  {type: 'default', size: 16},
+  {type: 'link', size: 16},
+  {type: 'small', size: 14},
+]
 
 export default function Typography() {
   return (
     <Container withInsets>
       <Content>
         <Header title="Typography" />
+        {TYPES.map(typo => (
+          <ThemedText key={typo.type} type={typo.type}>
+            {typo.size} {typo.type}
+          </ThemedText>
+        ))}
       </Content>
     </Container>
   )
--- components/ui/ThemedText.tsx
+++ components/ui/ThemedText.tsx
@@ -5,7 +5,7 @@ import {useThemeColor} from '@/hooks/useThemeColor'
 export type ThemedTextProps = TextProps & {
   lightColor?: string
   darkColor?: string
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link'
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'small' | 'link'
 }
 
 export function ThemedText({
@@ -26,6 +26,7 @@ export function ThemedText({
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
+        type === 'small' ? styles.small : undefined,
         style,
       ]}
       {...rest}
@@ -45,15 +46,20 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
     lineHeight: 32,
+    fontWeight: 'bold',
   },
   subtitle: {
     fontSize: 20,
+    lineHeight: 24,
     fontWeight: 'bold',
   },
+  small: {
+    fontSize: 14,
+    lineHeight: 24,
+  },
   link: {
-    lineHeight: 30,
+    lineHeight: 24,
     fontSize: 16,
     color: '#0a7ea4',
   },
